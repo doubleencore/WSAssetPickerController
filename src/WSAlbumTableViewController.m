88b1bee02c24c88d4e584d790dc0a5f0535d943f
Jonathan Huggins <jonathan@doubleencore.com>
2014-02-07 10:05:25 -0700

    Asset picker switched to videos and photos.

--- src/WSAlbumTableViewController.m
+++ src/WSAlbumTableViewController.m
@@ -127,7 +127,7 @@
     
     // Get the group from the datasource.
     ALAssetsGroup *group = [self.assetGroups objectAtIndex:indexPath.row];
-    [group setAssetsFilter:[ALAssetsFilter allPhotos]]; // TODO: Make this a delegate choice.
+    [group setAssetsFilter:[ALAssetsFilter allAssets]]; // TODO: Make this a delegate choice.
     
     // Setup the cell.
     cell.textLabel.text = [NSString stringWithFormat:@"%@ (%d)", [group valueForProperty:ALAssetsGroupPropertyName], [group numberOfAssets]];
@@ -143,7 +143,7 @@
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     ALAssetsGroup *group = [self.assetGroups objectAtIndex:indexPath.row];
-    [group setAssetsFilter:[ALAssetsFilter allPhotos]]; // TODO: Make this a delegate choice.
+    [group setAssetsFilter:[ALAssetsFilter allAssets]]; // TODO: Make this a delegate choice.
     
     WSAssetTableViewController *assetTableViewController = [[WSAssetTableViewController alloc] initWithStyle:UITableViewStylePlain];
     assetTableViewController.assetPickerState = self.assetPickerState;
